a27ab8f5511b09e19b8c3a64b0026ad29ac6c928
Rafiul Islam <rafiulratul21@gmail.com>
2019-06-20 19:59:17 +0600

    salary management

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { map } from 'rxjs/operators';
 })
 export class NavComponent {
 
-  private chosenView = 'salary management'  ;
+  private chosenView = 'add employee'  ;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
